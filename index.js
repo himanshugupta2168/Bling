13e7639a58d35b4f2ad16aecb7b2f2b78bc84f8a
himanshu2168.be21@chitkara.edu.in <himanshu2168.be21@chitkara.edu.in>
2023-10-31 02:20:03 +0530

    made configurations for connections woth cloudinary

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ require("dotenv").config();
 const port = process.env.PORT;
 const mongoose= require("mongoose");
 const database= require("./config/dbConnect")
+const cloudinary= require("./config/CloudinaryConnect")
+
+
+
+
+database.connect();
+cloudinary.cloudinaryConnect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/", (req,res)=>{
     res.send("hello welcome to the home page.  it is in under construction<br> See u soon😊😊 ");
@@ -12,8 +37,6 @@ app.get("/", (req,res)=>{
 app.listen(port , (err)=>{
     if (err){console.log(`error in listenning to port `);}
 })
-
-database.connect();
 
 
 
